f24c6fd03ee6ac8e7e1dc513d09c1c304f2dfef6
Donny-G <dennio_hus@icloud.com>
2021-07-22 13:34:01 +0300

    1-create-project: configure swiftlint

---
+++ VIPER_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER_Project
+//
+//  Created by Denis Golovizin on 22.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
